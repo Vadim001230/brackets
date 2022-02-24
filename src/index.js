2958ad50e4d31e49dd892cccb1d00b1486638778
Vadim Shershen <vadim.shershen.97@mail.ru>
2022-02-24 23:36:15 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let obj = Object.fromEntries(bracketsConfig);
+  let stack = [];
+  str = str.split('');
+  
+  for (let i of str) {
+    if (obj[stack[stack.length-1]] === i) {
+        stack.pop(); 
+      } else { 
+        stack.push(i);
+      }
+  }
+    return stack.length ? false : true;
 }
